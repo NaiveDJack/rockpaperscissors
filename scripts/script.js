4ce2a497959254b107e67878b91901bdd11245f8
NaiveDJack <naivedjack@protonmail.com>
2021-03-01 15:22:48 +0100

    Game function and main logic

--- scripts/script.js
+++ scripts/script.js
@@ -43,6 +43,10 @@ function round() {//plays out game mechanics
     let p2 = computerPlay();
     let result;
 
+    console.log("You played " + p1 + ".")
+    console.log("Computer plays " + p2 + ".")
+
+    //compares and determines outcome
     if (p1 === rock) {
         if (p2 == rock) {
             result = "tie";
@@ -77,10 +81,69 @@ function round() {//plays out game mechanics
         }
     }
 
+    //declares outcome through console
+    if (result === "win") {
+        console.log("You win this round!")
+    }
+    else if (result === "loss") {
+        console.log("The Computer wins this round!")
+    }
+    else if (result === "tie") {
+        console.log("It's a tie!")
+    }
+    else {
+        console.log("Uhm... what's going on here?")
+    }
+
     return result
 
 }
 
+function game() {//plays 5 rounds and determines a winner
+    let p1score = 0, p2score = 0;
+
+    console.log("Welcome to Rock Paper Scissors!")
+    console.log("You will face the Computer in a five round match")
+
+    //5 round and keeps score
+    bestOfThree: for (let step = 1; step < 6; ++step) {
+        
+        console.log("Round " + step)
+        let outcome = round();
+
+        if (outcome == "win") {
+            ++p1score
+            console.log("You have " + p1score + " points and the computer has " + p2score + " points.")
+            if (p1score > 2) {
+                break bestOfThree;
+            }
+        }
+        else if (outcome == "loss") {
+            ++p2score
+            console.log("You have " + p1score + " points and the computer has " + p2score + " points.")
+            if (p2score > 2) {
+                break bestOfThree;
+            }
+        }
+        else if (outcome == "tie") {
+            console.log("You have " + p1score + " points and the computer has " + p2score + " points.")
+        }
+    }
+
+    //checks score and declares winner
+    console.log("Match is over! The winner is...")
+
+    if (p1score > p2score) {
+        console.log("You! Congratulations!")
+    }
+    else if (p1score < p2score) {
+        console.log("The Computer! Glory to the Machines!")
+    }
+    else {
+        console.log("Both! We have a tie!")
+    }
+
+}
 
 //compare computerPlay with playerPlay
 //determine winner
