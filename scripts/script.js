41131d14857519822d69b50b46a1826f094ab42b
NaiveDJack <naivedjack@protonmail.com>
2021-04-01 15:47:39 +0200

    setup JS base DOM behaviour

--- scripts/script.js
+++ scripts/script.js
@@ -12,20 +12,27 @@ This script plays a game of RPS on your browser.
 //TODO: timing of the announcer, disabling/enabling buttons
 //I found easier to declare RPS as variables
 //just so autocomplete will help me write them
-const rock = 'rock',
-paper = 'paper',
-scissors = 'scissors';
+const rock = 'Rock',
+paper = 'Paper',
+scissors = 'Scissors';
 
+//buttons
 const rps = document.querySelectorAll('.rps');
 const newGame = document.querySelector('.new-game');
 let p1, p2;
 
-const output = document.querySelector('#output');
-let playerScore = Number(document.getElementById('player-score').textContent);
-let computerScore = Number(document.getElementById('computer-score').textContent);
+//divs
+let output = document.querySelector('#output'),
+playerScore = Number(document.getElementById('player-score').textContent),
+computerScore = Number(document.getElementById('computer-score').textContent),
+mainScreen = document.getElementById('main-screen'),
+startScreen = document.getElementById('start-screen'),
+gameScreen = document.getElementById('game-screen'),
+leftSide = document.getElementById('left-side');
 
+//text, all in the same place for consistency
 const welcome = "Welcome to Rock Paper Scissors!",
-tellRules = "You will face the Computer in a five round match",
+tellRules = "You will face the Computer in a five round match.",
 inputError = "Wrong input, sorry!",
 gameError = "Uhm... What's going on here?",
 gameOver = "The match is over, the winner is...",
@@ -35,6 +42,8 @@ tieGame = "Both! We have a tie!",
 winRound = "You win this round!",
 loseRound = "The Computer wins this round!",
 tieRound = "It's a tie!";
+
+gameScreen = mainScreen.removeChild(gameScreen);
 
 function computerPlay() {
     let play = Math.round(Math.random() * (3-1) + 1);
@@ -48,6 +57,7 @@ function computerPlay() {
     else if (play === 3) {
         p2 = scissors;
     }
+
     return(p2)
 }
 
@@ -70,6 +80,7 @@ function playerPlay(p1) {
 
 function round(p1, computerPlay) {
     let result;
+    leftSide = gameScreen.removeChild(leftSide); 
 
     if (p1 === rock) {
         if (p2 == rock) {
@@ -106,17 +117,17 @@ function round(p1, computerPlay) {
     }
 
     if (result === "win") {
-        announcer(winRound);
+        announcer("You play " + p1 + "!", "Computer plays " + p2 + "!", winRound);
         playerScore += 1
         document.getElementById('player-score').innerText = playerScore;
     }
     else if (result === "loss") {
-        announcer(loseRound);
+        announcer("You play " + p1 + "!", "Computer plays " + p2 + "!", loseRound);
         computerScore +=1
         document.getElementById('computer-score').innerText = computerScore;
     }
     else if (result === "tie") {
-        announcer(tieRound);
+        announcer("You play " + p1 + "!", "Computer plays " + p2 + "!", tieRound);
         playerScore += 1
         computerScore +=1
         document.getElementById('player-score').innerText = playerScore;
@@ -127,44 +138,44 @@ function round(p1, computerPlay) {
     }
 
     if (playerScore == 5 || computerScore == 5) {
-        gameEnd();
+        setTimeout( () => {
+            gameEnd();
+        }, 1000) 
     }
     else {
-        //unlocks rps buttons
+        setTimeout( () => {
+            gameScreen.appendChild(leftSide);
+        } , 1000)
     }
-
 }
 
 function gameStart() {
-
-    //resets the screen to initial values
+    mainScreen.removeChild(startScreen);
+    mainScreen.appendChild(gameScreen);
 
     playerScore = 0
     computerScore = 0
     document.getElementById('player-score').innerText = playerScore;
     document.getElementById('computer-score').innerText = computerScore;
     announcer(welcome, tellRules)
-
-    //unlocks rps buttons
-
 }
 
 function gameEnd() {
 
-    //locks rps buttons
-    announcer(gameOver);
-
     if (playerScore > computerScore) {
-        announcer(winGame);
+        announcer(gameOver, winGame);
     }
     else if (playerScore < computerScore) {
-        announcer(loseGame);
+        announcer(gameOver, loseGame);
     }
     else {
-        announcer(tieGame);
+        announcer(gameOver, tieGame);
     }
 
-    //unlock new game button
+    setTimeout(() => {
+        mainScreen.removeChild(gameScreen);
+        mainScreen.appendChild(startScreen);
+    }, 3000)
 }
 
 function announcer(...what) {
@@ -172,8 +183,10 @@ function announcer(...what) {
         output.removeChild(output.firstChild);
     }
     what.forEach(element => {
-        output.append(element)
-        //times and fades in the elements
+        let utterance = document.createElement("P");
+        let text = document.createTextNode(element);
+        output.appendChild(text);
+        document.getElementById("output").appendChild(utterance);
     });
     
 }
